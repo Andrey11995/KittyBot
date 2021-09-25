ac88cad2c3a3c780b633f7901219a232dea770b5
Andrey11995 <drunya119955@yandex.ru>
2021-09-25 12:13:36 +0300

    Try webhooks

--- kittybot.py
+++ kittybot.py
@@ -41,53 +41,50 @@ def webhook():
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    name = message.from_user.first_name
-    bot.reply_to(
-        message,
-        'Привет, {}. Посмотри, какого котика я тебе нашёл!'.format(name)
-    )
+    # name = message.from_user.first_name
+    bot.reply_to(message, 'Привет, ' + message.from_user.first_name)      # , {}. Посмотри, какого котика я тебе нашёл!'.format(name)
 
 
-def get_new_image():
-    try:
-        response = requests.get(API_URL).json()
-    except Exception as error:
-        logger.warning('API котиков не отвечает!')
-        logger.error(error)
-        logger.info('Пытаемся запросить собачек')
-        new_url = 'https://api.thedogapi.com/v1/images/search'
-        response = requests.get(new_url).json()
-    random_cat = response[0].get('url')
-    return random_cat
+# def get_new_image():
+#     try:
+#         response = requests.get(API_URL).json()
+#     except Exception as error:
+#         logger.warning('API котиков не отвечает!')
+#         logger.error(error)
+#         logger.info('Пытаемся запросить собачек')
+#         new_url = 'https://api.thedogapi.com/v1/images/search'
+#         response = requests.get(new_url).json()
+#     random_cat = response[0].get('url')
+#     return random_cat
+#
+#
+# def new_cat(update, context):
+#     try:
+#         chat = update.effective_chat
+#         context.bot.send_photo(chat.id, get_new_image())
+#         logger.info('Фото отправлено')
+#     except Exception as error:
+#         logger.error(f'Не удалось отправить сообщение! Ошибка: {error}')
+#
+#
+# def wake_up(update, context):
+#     try:
+#         chat = update.effective_chat
+#         name = update.message.chat.first_name
+#         button = ReplyKeyboardMarkup([['/newcat']], resize_keyboard=True)
+#         context.bot.send_message(
+#             chat_id=chat.id,
+#             text='Привет, {}. Посмотри, какого котика я тебе нашёл!'.format(name),
+#             reply_markup=button
+#         )
+#         context.bot.send_photo(chat.id, get_new_image())
+#         logger.info('Фото отправлено')
+#     except Exception as error:
+#         logger.error(f'Не удалось отправить сообщение! Ошибка: {error}')
 
 
-def new_cat(update, context):
-    try:
-        chat = update.effective_chat
-        context.bot.send_photo(chat.id, get_new_image())
-        logger.info('Фото отправлено')
-    except Exception as error:
-        logger.error(f'Не удалось отправить сообщение! Ошибка: {error}')
-
-
-def wake_up(update, context):
-    try:
-        chat = update.effective_chat
-        name = update.message.chat.first_name
-        button = ReplyKeyboardMarkup([['/newcat']], resize_keyboard=True)
-        context.bot.send_message(
-            chat_id=chat.id,
-            text='Привет, {}. Посмотри, какого котика я тебе нашёл!'.format(name),
-            reply_markup=button
-        )
-        context.bot.send_photo(chat.id, get_new_image())
-        logger.info('Фото отправлено')
-    except Exception as error:
-        logger.error(f'Не удалось отправить сообщение! Ошибка: {error}')
-
-
-def main():
-    logger.debug('КотоБот запущен')
+# def main():
+#     logger.debug('КотоБот запущен')
 
     # updater = Updater(token=TELEGRAM_TOKEN)
     #
@@ -100,4 +97,3 @@ def main():
 
 if __name__ == '__main__':
     server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-    main()
